887d2a72de4653284e7aad9ea133f7e387420266
Asees-Khurana1 <asees23024@iiitd.ac.in>
2024-04-18 10:00:11 +0530

    common FileEntity file

--- app/src/main/java/com/asees/databackupapp/FileEntity.kt
+++ app/src/main/java/com/asees/databackupapp/FileEntity.kt
@@ -2,13 +2,15 @@ package com.asees.databackupapp
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.firebase.database.IgnoreExtraProperties
 
 @Entity(tableName = "files")
+@IgnoreExtraProperties  // Firebase annotation to ignore fields not listed
 data class FileEntity(
-    @PrimaryKey var id: String,
-    val fileName: String,
-    val filePath: String,
-    var lastAccessed: Long,
-    var isBackedUp: Boolean,
-    var frequentlyUsed: Boolean = false
+    @PrimaryKey val id: String = "",  // Use a default value for compatibility with Firebase
+    var fileName: String = "",
+    var filePath: String = "",
+    var lastAccessed: Long = 0,
+    var isBackedUp: Boolean = false,
+    var frequentlyUsed: Boolean = false  // Example additional field
 )
