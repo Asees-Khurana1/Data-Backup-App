f89b8d76da22e778f14a6207a9ce183e41b5a15a
Asees-Khurana1 <143724831+Asees-Khurana1@users.noreply.github.com>
2024-05-11 19:35:41 +0530

    mainactivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,8 @@ dependencies {
     implementation("androidx.compose.material:material:1.6.5")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.ui:ui-tooling:1.6.5")
+    implementation("androidx.compose.material3:material3:1.0.0-alpha02")
+
 
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
